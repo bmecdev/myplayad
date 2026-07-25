7435148ed9c8b4aa6a5496663014bf40bf969a4c
gonza <gbotero@boteromechatronics.com>
2026-07-25 12:18:28 -0500

    fix: add ts-ignore and experimental config for next js

--- portal/next.config.ts
+++ portal/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
+// @ts-ignore
 const nextConfig: NextConfig = {
   serverExternalPackages: [],
   serverActions: {
     bodySizeLimit: '500mb',
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '500mb',
+    }
+  }
 };
 
 export default nextConfig;
